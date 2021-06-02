29956cdf4e7b6f4e5c0b9a03c25d8e01fe118b2c
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-02 08:58:51 +0800

    First commit, error installing react-router-dom

---
+++ src/components/Dummy2/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+	return <div>I'm from components/Dummy2</div>;
+};
+
+export default Index;
---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dummy2 from 'components/Dummy2';
+
+const LoginPage = () => {
+	return (
+		<div>
+			<Dummy2 />
+		</div>
+	);
+};
+
+export default LoginPage;
